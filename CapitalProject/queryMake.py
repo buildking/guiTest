9022f659643ae94668075a3db89fd642e5a2536f
gf160 <gf160@naver.com>
2021-10-02 11:27:28 +0900

    config파일 주석 추가 쿼리 만들기 기능 추가

--- CapitalProject/queryMake.py
+++ CapitalProject/queryMake.py
@@ -2,6 +2,8 @@ import pandas as pd
 import config
 import cx_Oracle
 import log
+import os
+from zipfile import ZipFile
 
 #로그파일 세팅
 logger = log.setLogging("queryMake")
@@ -11,15 +13,29 @@ logger.debug("config file read")
 _config = config.ConfigUtil()
 _oracleSetting = _config.getOracleSetting()
 
+def unzipOracleClient():
+    #압축이 풀릴 디렉토리명
+    logger.info("instantclient_19_12/ create!")
+
+    with ZipFile('./instantclient-basic-windows.x64-19.12.0.0.0dbru.zip', 'r') as zip:
+        zip.extractall()
+        logger.info('oracleclient.zip file is unzipped in "instantclient_19_12/" folder')
+
 def queryMake():
+    logger.info("query make start")
     beforeExcelToQuery()
     logger.info("")
-    logger.info("--->before is Done<---")
+    logger.info("--->excel_before convert complete<---")
     logger.info("")
     afterExcelToQuery()
     logger.info("")
-    logger.info("--->after is Done<---")
+    logger.info("--->excel_after convert complete<---")
     logger.info("")
+
+    if _oracleSetting['database_insert'] == 'Y':
+        #DBinsert하도록 되어있으면
+        oracleInsert()
+    logger.info("query make end")
 
 def beforeExcelToQuery():
 
@@ -37,31 +53,20 @@ def beforeExcelToQuery():
         logger.debug("no." + str(index) + " row read!")
         # N번쨰 row 가져오기
         # 각 컬럼이 16개나 되기때문에 index로 접근하기위해 .loc사용
-        col1 = beforeDf.loc[index][0]
-        col2 = beforeDf.loc[index][1]
-        col3 = beforeDf.loc[index][2]
-        col4 = beforeDf.loc[index][3]
-        col5 = beforeDf.loc[index][4]
-        col6 = beforeDf.loc[index][5]
-        col7 = beforeDf.loc[index][6]
-        col8 = beforeDf.loc[index][7]
-        col9 = beforeDf.loc[index][8]
-        col10 = beforeDf.loc[index][9]
-        col11 = beforeDf.loc[index][10]
-        col12 = beforeDf.loc[index][11]
-        col13 = beforeDf.loc[index][12]
-        col14 = beforeDf.loc[index][13]
-        col15 = beforeDf.loc[index][14]
-        col16 = beforeDf.loc[index][15]
+        col0 = beforeDf.loc[index][0]#연번
+        col4 = beforeDf.loc[index][4].strip()#피보험자
+        col5 = beforeDf.loc[index][5].strip()#피보험 주민번호
+        col10 = beforeDf.loc[index][10]#계약체결일
+        col11 = beforeDf.loc[index][11].strip()#계약소멸일
+        col13 = beforeDf.loc[index][13].strip()#모집인명
+        col14 = beforeDf.loc[index][14].strip()#모집인주민번호
 
         # insert 쿼리 생성
-        a = f'''INSERT INTO {beforeTableName}( col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16)
-            VALUE('{col1}', '{col2}','{col3}','{col4}','{col5}','{col6}','{col7}','{col8}','{col9}','{col10}','{col11}','{col12}','{col13}','{col14}','{col15}','{col16}');
+        a = f'''INSERT INTO {beforeTableName}( "number_old", EXP_DATE, INS_NM, INS_BIRTH, PLNR_NM, PLNR_BIRTH)VALUES('{col0}', '{col11}','{col4}','{col5}','{col13}','{col14}');
         '''
 
         # insert 쿼리 파일에 write
         file.write(a)
-        file.write("\n")
         logger.info("before:: no." + str(index+1) + " row file write")
 
     logger.info("before_excel to sql success")
@@ -83,47 +88,83 @@ def afterExcelToQuery():
         logger.debug("no." + str(index) + " row read!")
         # N번쨰 row 가져오기
         # 각 컬럼이 16개나 되기때문에 index로 접근하기위해 .loc사용
-        col1 = afterDf.loc[index][0]
-        col2 = afterDf.loc[index][1]
-        col3 = afterDf.loc[index][2]
-        col4 = afterDf.loc[index][3]
-        col5 = afterDf.loc[index][4]
-        col6 = afterDf.loc[index][5]
-        col7 = afterDf.loc[index][6]
-        col8 = afterDf.loc[index][7]
-        col9 = afterDf.loc[index][8]
-        col10 = afterDf.loc[index][9]
-        col11 = afterDf.loc[index][10]
-        col12 = afterDf.loc[index][11]
-        col13 = afterDf.loc[index][12]
-        col14 = afterDf.loc[index][13]
-        col15 = afterDf.loc[index][14]
-        col16 = afterDf.loc[index][15]
+        col0 = afterDf.loc[index][0]  # 연번
+        col4 = afterDf.loc[index][4].strip()  # 피보험자
+        col5 = afterDf.loc[index][5].strip()  # 피보험 주민번호
+        col10 = afterDf.loc[index][10]  # 계약체결일
+        col11 = afterDf.loc[index][11].strip()  # 계약소멸일
+        col13 = afterDf.loc[index][13].strip()  # 모집인명
+        col14 = afterDf.loc[index][14].strip()  # 모집인주민번호
 
         #insert 쿼리 생성
-        a = f'''INSERT INTO {afterTableName}( col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16)
-            VALUE('{col1}', '{col2}','{col3}','{col4}','{col5}','{col6}','{col7}','{col8}','{col9}','{col10}','{col11}','{col12}','{col13}','{col14}','{col15}','{col16}');
+        a = f'''INSERT INTO {afterTableName}("number_new", CON_DATE, INS_NM, INS_BIRTH, PLNR_NM, PLNR_BIRTH)VALUES('{col0}','{col10}','{col4}','{col5}','{col13}','{col14}');
         '''
 
         #insert쿼리 파일에 write
         file.write(a)
-        file.write("\n")
         logger.info("after:: no." + str(index+1) + " row file write")
 
     logger.info("after_excel to sql success")
     file.close()
 
 
-def oracleConnect():
-    dsn = cx_Oracle.makedsn("IP", "PORT:int", "SID")
-    conn = cx_Oracle.connect("USER_ID", "PASSWORD", dsn)
+def oracleInsert():
+    logger.info("oracle db insert is start")
+
+    # instantclient_19_12/가 없다면
+    if not os.path.isdir("./instantclient_19_12"):
+        logger.info("instantclient_19_12/ is not exist")
+        unzipOracleClient()
+    else:
+        # instantclient_19_12/가 비어있다면
+        if len(os.listdir("./instantclient_19_12")) == 0:
+            logger.info("instantclient_19_12/ is empty")
+            unzipOracleClient()
+
+    # 오라클 클라이언트 디렉토리 (상대경로 -> 절대경로)
+    oracleClientDir = './instantclient_19_12'
+    oracleClientDir_abs = os.path.abspath(oracleClientDir)
+
+    # 환경변수 설정(oracle 접속을위해 필요
+    LOCATION = oracleClientDir
+    os.environ["PATH"] = LOCATION + ";" + os.environ["PATH"]
+
+    #설정파일 변수에 저장
+    _ip = _oracleSetting["ip"]
+    _port = _oracleSetting["port"]
+    _service = _oracleSetting["service"]
+    _userName = _oracleSetting["user_name"]
+    _userPwd = _oracleSetting["user_password"]
+
+    #오라클 접속
+    #dsn = cx_Oracle.makedsn(_ip, int(_port), _service)
+    #conn = cx_Oracle.connect(_userName, _userPwd, dsn)
+    dsn = cx_Oracle.makedsn("211.238.147.202", 1521, "netis")
+    conn = cx_Oracle.connect("SK2019", "ntflow", dsn)
+    logger.debug("database connect!")
 
     cursor = conn.cursor()
-    cursor.execute("select 1 from dual")
+    #기존 데이터 삭제
+    cursor.execute("DELETE FROM END_CONTRACT")
+    cursor.execute("DELETE FROM NEW_CONTRACT")
+    conn.commit()
 
-    for data in cursor:
-        print(data)
+    #sql파일을 line 단위로 읽어서 실행한다.
+    file1_lines = open("./sql/before.sql", encoding='utf8').readlines()
+    for line in file1_lines:
+        if line.strip():
+            cursor.execute(line.strip().replace(";", ""))
+    conn.commit()
+
+    file2_lines = open("./sql/after.sql", encoding='utf8').readlines()
+    for line in file2_lines:
+        if line.strip():
+            cursor.execute(line.strip().replace(";", ""))
+    conn.commit()
 
     conn.close()
+    logger.info("oracle db insert is done")
 
-queryMake()
+#queryMake()
+
+oracleInsert()
