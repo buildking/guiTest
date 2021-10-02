0b4bf0859d684df92d661b46f5b4083b779fb36f
gf160 <gf160@naver.com>
2021-10-02 11:30:07 +0900

    쿼리 삭제(보안)

--- CapitalProject/sql/after.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO NEW_CONTRACT("number_new", CON_DATE, INS_NM, INS_BIRTH, PLNR_NM, PLNR_BIRTH)VALUES('111','2019-12-31','홍길동','910911-*******','김철수','891023-*******');
-        INSERT INTO NEW_CONTRACT("number_new", CON_DATE, INS_NM, INS_BIRTH, PLNR_NM, PLNR_BIRTH)VALUES('222','2016-01-20','주시경','190102-*******','김철수','891023-*******');
-        INSERT INTO NEW_CONTRACT("number_new", CON_DATE, INS_NM, INS_BIRTH, PLNR_NM, PLNR_BIRTH)VALUES('333','2020-01-01','토마스','671010-*******','김철수','891023-*******');
-        
--- CapitalProject/sql/before.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO END_CONTRACT( "number_old", EXP_DATE, INS_NM, INS_BIRTH, PLNR_NM, PLNR_BIRTH)VALUES('111', '2020-12-31','홍길동','910911-*******','김철수','891023-*******');
-        INSERT INTO END_CONTRACT( "number_old", EXP_DATE, INS_NM, INS_BIRTH, PLNR_NM, PLNR_BIRTH)VALUES('222', '2020-12-31','주시경','190102-*******','김철수','891023-*******');
-        INSERT INTO END_CONTRACT( "number_old", EXP_DATE, INS_NM, INS_BIRTH, PLNR_NM, PLNR_BIRTH)VALUES('333', '2020-12-31','토마스','671010-*******','김철수','891023-*******');
-        
