5b4c33b566a1ac16019811208bf80a93b92e0ae5
buildking <qtbelt@gmail.com>
2021-10-04 21:01:42 +0900

    파일 읽기+합치기, 파일 비교하기

--- CapitalProject/AddFile.py
+++ CapitalProject/AddFile.py
@@ -5,47 +5,48 @@ import pandas as pd
 
 root = Tk()
 
-#####################################파일 읽어오기##################################
 
-#before read
-beforeFileList = listdir('excel_before')
 
-print(beforeFileList)
 
-readExcel = []
 
-for tmp in beforeFileList:
 
-    tmpExcel = pd.read_excel(f'./excel_before/{tmp}')
 
-    readExcel.append(tmpExcel)
 
-#print(readExcel)
-
-#after read
-beforeFileList2 = listdir('excel_after')
-
-#print(beforeFileList2)
-
-readExcel2 = []
-
-for tmp2 in beforeFileList2:
-
-    tmpExcel2 = pd.read_excel(f'./excel_after/{tmp2}')
-
-    readExcel2.append(tmpExcel2)
-
-#print(readExcel2)
-
-####################################################################################################################
-
-#에러메시지 출력(프로세스는 조금 더 고민해야될듯)
-def combineError():
-    pass
 
 #엑셀 합치기
 def excelCombine():
-    global readExcel
+
+    #합칠 파일이 부족할 때 경고문
+    def combineError():
+        msbox.showwarning("파일 개수 부족", "합칠 엑셀파일이 부족합니다.")
+
+
+
+#####################################파일 읽어오기##################################
+
+    # before read
+    beforeFileList = listdir('excel_before')
+
+    print(beforeFileList)
+
+    readExcel = []
+
+    for tmp in beforeFileList:
+        tmpExcel = pd.read_excel(f'./excel_before/{tmp}')
+
+        readExcel.append(tmpExcel)
+
+    # after read
+    beforeFileList2 = listdir('excel_after')
+
+    readExcel2 = []
+
+    for tmp2 in beforeFileList2:
+        tmpExcel2 = pd.read_excel(f'./excel_after/{tmp2}')
+
+        readExcel2.append(tmpExcel2)
+
+##########################################################################
 
     #before merge
     if len(readExcel) == 0 or len(readExcel) == 1:
@@ -66,20 +67,23 @@ def excelCombine():
     combineExcel2.to_excel('./excel_result/excel_after.xlsx', index=False)
 
 
+
+
+
+
 #before, after excel file compare
 
-#dataframe declare
-beforeDataFrame = pd.DataFrame()
-afterDataFrame = pd.DataFrame()
-
-beforeCore = pd.DataFrame()
-afterCore = pd.DataFrame()
-
-finalResultBefore = pd.DataFrame()
-finalResultAfter = pd.DataFrame()
-
 def excelCompare():
-    global beforeDataFrame, afterDataFrame, lastNum
+
+    # dataframe declare
+    beforeDataFrame = pd.DataFrame()
+    afterDataFrame = pd.DataFrame()
+
+    beforeCore = pd.DataFrame()
+    afterCore = pd.DataFrame()
+
+    finalResultBefore = pd.DataFrame()
+    finalResultAfter = pd.DataFrame()
 
     beforeDataFrame = pd.read_excel('./excel_result/excel_before.xlsx')
     afterDataFrame = pd.read_excel('./excel_result/excel_after.xlsx')
