6ffcc2733f76633f5c5c652790c85440e26aca5d
buildking <qtbelt@gmail.com>
2021-10-04 20:36:44 +0900

    로직은 다 짰음. 마무리만 하면 끝.

--- CapitalProject/AddFile.py
+++ CapitalProject/AddFile.py
@@ -114,14 +114,34 @@ def excelCompare():
     lastNum = []
     lastNum.clear()
 
+    #조건을 통과한 값의 b'계약소멸일'-a'계약체결일'이 180이내인지 추려냄
     for i, j in coreRowNum:
         #날짜 차이가 180일 이내인지 비교
-        if int(str(pd.to_datetime(str(afterDataFrame.loc[j,'계약체결일']))-pd.to_datetime(str(beforeDataFrame.loc[i,'계약체결일']))).split()[0]) < 181 :
-            lastNum.append([i,j])
+        if abs(int(str(pd.to_datetime(str(afterDataFrame.loc[j,'계약체결일']))-pd.to_datetime(str(beforeDataFrame.loc[i,'계약소멸일']))).split()[0])) < 181 :
+            #aSubB = abs(a[계약체결일] - b[계약소멸일])
+            aSubB = int(str(pd.to_datetime(str(afterDataFrame.loc[j, '계약체결일'])) - pd.to_datetime(
+                str(beforeDataFrame.loc[i, '계약소멸일']))).split()[0])
+            lastNum.append([i,j, aSubB])
 
-    lastExcel = pd.DataFrame(lastNum, columns=['beforeExcel','afterExcel'])
+    lastExcel = pd.DataFrame(lastNum, columns=['beforeExcel','afterExcel', '차이'])
     print(lastNum)
-    lastExcel.to_excel('./excel_final/excel_final.xlsx', index=False)
+    lastExcel.to_excel('./excel_final/excel_final_rows.xlsx', index=False)
+
+    #최종적으로 걸러진 row값을 이용해 excel_final 생성
+    finalExcel = pd.DataFrame(columns=[['소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '차이'],['회사명', '피보험자', '증권번호', '상품명', '상품분류', '계약소멸일', '상태', '회사명', '피보험자', '증권번호', '상품명', '상품분류', '계약체결일', '상태', '모집인명', '생년월일', '차이']])
+
+    x = 0
+    for b, a, diff in lastNum:
+        x += 1
+        finalExcel.loc[x] = (beforeDataFrame.loc[b, '회사명'], beforeDataFrame.loc[b, '피보험자'], beforeDataFrame.loc[b, '증권번호'], beforeDataFrame.loc[b, '상품명'], beforeDataFrame.loc[b, '상품분류'], beforeDataFrame.loc[b, '계약소멸일'], beforeDataFrame.loc[b, '계약상태'], afterDataFrame.loc[a, '회사명'], afterDataFrame.loc[a, '피보험자'], afterDataFrame.loc[a, '증권번호'], afterDataFrame.loc[a, '상품명'], afterDataFrame.loc[a, '상품분류'], afterDataFrame.loc[a, '계약체결일'], afterDataFrame.loc[a, '계약상태'], afterDataFrame.loc[a, '모집인명'], afterDataFrame.loc[a, '피보험자\n주민등록번호'], diff)
+
+    finalExcel.to_excel('./excel_final/excel_final.xlsx')
+
+
+
+
+
+
 
 
 
