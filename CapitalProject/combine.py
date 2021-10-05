fed02125739377d82dddbbccfee69c50cd41eee4
buildking <qtbelt@gmail.com>
2021-10-05 22:29:21 +0900

    엑셀 합치기 연번 새로 적용 안되는점 수정.

--- CapitalProject/combine.py
+++ CapitalProject/combine.py
@@ -11,9 +11,6 @@ def excelCombine():
         msbox.showwarning("파일 개수 부족", f"{state}폴더에 합칠 엑셀파일이 부족합니다.")
 
 
-
-#####################################파일 읽어오기##################################
-
     # before read
     beforeFileList = listdir('excel_before')
 
@@ -49,25 +46,81 @@ def excelCombine():
 
             msbox.showwarning("파일 인식 불가", "excel_after폴더를 확인해주세요.")
 
-    ##########################################################################
-    print('*'*20,beforeExcelList,'*'*20)
-    print('*' * 20, afterExcelList, '*' * 20)
+
+
     #before merge
+
+    #폴더 안에 파일이 없거나 하나뿐이라면 에러메세지 띄우기
     if len(beforeExcelList) == 0 or len(beforeExcelList) == 1:
         combineError('before')
         return
+
     combineBeforeExcel = pd.DataFrame()
+
+    #첫번째 엑셀파일을 데이터프레임에 삽입
     combineBeforeExcel = beforeExcelList[0]
+
+    #폴더 안에 있는 엑셀 리스트들을 차례대로 병합
     for beforeExcelOne in beforeExcelList[1:]:
         combineBeforeExcel = pd.concat([beforeExcelOne, combineBeforeExcel], join='outer')
+
+    # 만들어진 엑셀 행*열 개수 tuple형태로 저장
+    combineBeforeExcelShape = combineBeforeExcel.shape
+
+    #기존 '연번' 컬럼 삭제
+    combineBeforeExcel = combineBeforeExcel.drop(columns='연번', axis=1)
+
+    #새로운 '연번'에 들어갈 list 생성
+    beForeIndexList = []
+    beForeIndexList.clear()
+
+    #엑셀 row 수 세기
+    x = 0
+    for index in range(combineBeforeExcelShape[0]):
+        x += 1
+        beForeIndexList.append(x)
+
+    #새로 생성한 '연번'을 첫번째 칼럼으로 삽입
+    combineBeforeExcel.insert(0, '연번', beForeIndexList)
+    #before파일 엑셀로 만들기
     combineBeforeExcel.to_excel('./excel_result/excel_before.xlsx', index=False)
 
+
+
     #after merge
+
+    #폴더 안에 파일이 없거나 하나뿐이라면 에러메세지 띄우기
     if len(afterExcelList) == 0 or len(afterExcelList) == 1:
         combineError('after')
         return
+
     combineAfterExcel = pd.DataFrame()
+
+    #첫번째 엑셀파일을 데이터프레임에 삽입
     combineAfterExcel = afterExcelList[0]
+
+    #폴더 안에 있는 엑셀 리스트들을 차례대로 병합
     for afterExcelOne in afterExcelList[1:]:
         combineAfterExcel = pd.concat([afterExcelOne, combineAfterExcel], join='outer')
+
+    # 만들어진 엑셀 행*열 개수 tuple형태로 저장
+    combineAfterExcelShape = combineAfterExcel.shape
+
+    #기존 '연번' 컬럼 삭제
+    combineAfterExcel = combineAfterExcel.drop(columns='연번', axis=1)
+
+
+    #새로운 '연번'에 들어갈 list 생성
+    afterIndexList = []
+    afterIndexList.clear()
+
+    #엑셀 row 수 세기
+    x = 0
+    for index in range(combineAfterExcelShape[0]):
+        x += 1
+        afterIndexList.append(x)
+
+    #새로 생성한 '연번'을 첫번째 칼럼으로 삽입
+    combineAfterExcel.insert(0, '연번', afterIndexList)
+    #after파일 엑셀로 만들기
     combineAfterExcel.to_excel('./excel_result/excel_after.xlsx', index=False)
--- CapitalProject/compare.py
+++ CapitalProject/compare.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 #before, after excel file compare
-
 def excelCompare():
 
     # dataframe declare
@@ -17,18 +16,9 @@ def excelCompare():
     beforeDataFrame = pd.read_excel('./excel_result/excel_before.xlsx')
     afterDataFrame = pd.read_excel('./excel_result/excel_after.xlsx')
 
-    # print(beforeDataFrame)
-    # print(afterDataFrame)
-
     #비교 통과한 before data와 after data의 row 저장(append로)
     coreRowNum = []
     coreRowNum.clear()
-
-
-    # for pdb, rowBefore in beforeDataFrame.iterrows():
-    #     print('되냐??'*10)
-    #     print(pdb,'뎃데로게'*10, rowBefore)
-    #     print('야지막으로, ', rowBefore['피보험자\n주민등록번호'])
 
     for pdb, rowBefore in beforeDataFrame.iterrows():
         print(pdb, rowBefore)
@@ -40,12 +30,10 @@ def excelCompare():
                         if str(rowBefore['모집인 주민번호'])[:5] == str(rowAfter['모집인 주민번호'])[:5]:
                             coreRowNum.append([pdb, pda])
 
-    print('*'*50, coreRowNum, '*'*50)
-
     lastList = []
     lastList.clear()
 
-    #조건을 통과한 값의 b'계약소멸일'-a'계약체결일'이 180이내인지 추려냄
+    #조건을 통과한 값의 (b'계약소멸일' - a'계약체결일')이 180이내인지 추려냄
     for i, j in coreRowNum:
         #날짜 차이가 180일 이내인지 비교
         if abs(int(str(pd.to_datetime(str(afterDataFrame.loc[j,'계약체결일']))-pd.to_datetime(str(beforeDataFrame.loc[i,'계약소멸일']))).split()[0])) < 181 :
@@ -56,7 +44,7 @@ def excelCompare():
 
     lastExcel = pd.DataFrame(lastList, columns=['beforeExcel','afterExcel', '차이'])
     print(lastList)
-    lastExcel.to_excel('./excel_final/excel_final_rows.xlsx', index=False)
+    lastExcel.to_excel('./excel_result/excel_final_rows.xlsx', index=False)
 
     #최종적으로 걸러진 row값을 이용해 excel_final 생성
     finalExcel = pd.DataFrame(columns=[['소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '소멸계약<이동 전>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '신규계약<이동 후>', '소속', '차이'],['회사명', '피보험자', '증권번호', '상품명', '상품분류', '계약소멸일', '상태', '회사명', '피보험자', '증권번호', '상품명', '상품분류', '계약체결일', '상태', '모집인명', '생년월일', '소속', '차이']])
@@ -66,5 +54,5 @@ def excelCompare():
         x += 1
         finalExcel.loc[x] = (beforeDataFrame.loc[b, '회사명'], beforeDataFrame.loc[b, '피보험자'], beforeDataFrame.loc[b, '증권번호'], beforeDataFrame.loc[b, '상품명'], beforeDataFrame.loc[b, '상품분류'], beforeDataFrame.loc[b, '계약소멸일'], beforeDataFrame.loc[b, '계약상태'], afterDataFrame.loc[a, '회사명'], afterDataFrame.loc[a, '피보험자'], afterDataFrame.loc[a, '증권번호'], afterDataFrame.loc[a, '상품명'], afterDataFrame.loc[a, '상품분류'], afterDataFrame.loc[a, '계약체결일'], afterDataFrame.loc[a, '계약상태'], afterDataFrame.loc[a, '모집인명'], afterDataFrame.loc[a, '피보험자\n주민등록번호'], afterDataFrame.loc[a, '모집인 소속\n(이동 후)'], diff)
 
-    finalExcel.to_excel('./excel_final/excel_final.xlsx')
+    finalExcel.to_excel('./excel_result/excel_final.xlsx')
 
