b0273a581e64c21fd8966098932bdb2677a0af1e
gf160 <gf160@naver.com>
2021-11-20 14:04:02 +0900

    sql파일 가독성 증가

--- CapitalProject/sql/table.sql
+++ CapitalProject/sql/table.sql
@@ -1,5 +1,5 @@
-create table if not exists END_CONTRACT (number_old text, EXP_DATE text, INS_NM text, INS_BIRTH text, PLNR_NM text, PLNR_BIRTH text);
-create table if not exists NEW_CONTRACT (number_new text, CON_DATE text, INS_NM text, INS_BIRTH text, PLNR_NM text, PLNR_BIRTH text);
+CREATE TABLE IF NOT EXISTS END_CONTRACT(number_old text, EXP_DATE text, INS_NM text, INS_BIRTH text, PLNR_NM text, PLNR_BIRTH text);
+CREATE TABLE IF NOT EXISTS NEW_CONTRACT(number_new text, CON_DATE text, INS_NM text, INS_BIRTH text, PLNR_NM text, PLNR_BIRTH text);
 
 
 
