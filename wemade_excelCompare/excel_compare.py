8d845abcdbba7ad5633d8e0282e69d744d6b2f6e
buildking <qtbelt@gmail.com>
2021-12-06 23:55:38 +0900

    미완성품 테스트 전 저장

--- wemade_excelCompare/excel_compare.py
+++ wemade_excelCompare/excel_compare.py
@@ -1,4 +1,4 @@
-import openpyxl
+from openpyxl.styles import painting
 from tkinter import *
 import pandas as pd
 import log
@@ -20,68 +20,69 @@ def xlCompare(_text=None):
     _text.insert(END, f"\n[{time}] 엑셀파일을 불러옵니다.\n")
 
     #폴더 안 파일의 리스트 생성
-    FileList = listdir('input')
-    logger.info(FileList)
-    print(FileList)
+    excelList = listdir('input')
+    logger.info(excelList)
+    print(excelList)
     time = str(datetime.datetime.now())[0:-7]
-    _text.insert(END, f"[{time}] 파일 리스트\n{FileList}\n")
+    _text.insert(END, f"[{time}] 파일 리스트\n{excelList}\n")
 
     #폴더 안에 파일이 2개가 아니라면 에러메세지 띄우기
-    if len(FileList) != 2:
+    if len(excelList) != 2:
         shortError('input')
         return
 
     #리스트 안의 파일 하나씩 꺼내기
     #첫번째 파일
     try:
-        endexcel = pd.DataFrame()
+        endFrame = pd.DataFrame()
         #엑셀 모든 시트를 합쳐서 받는다.
-        endexcel = pd.read_excel(f'./input/{FileList[0]}', sheet_name=None, dtype=str)
+        endFrame = pd.read_excel(f'./input/{excelList[0]}', sheet_name=None, dtype=str)
     except:
-
         #엑셀파일 인식이 안된다면, 에러메세지 띄우기
-        msbox.showwarning("파일 인식 불가", f"input폴더의 {FileList[0]}파일을 확인해주세요.")
+        msbox.showwarning("파일 인식 불가", f"input폴더의 {excelList[0]}파일을 확인해주세요.")
         logger.debug("excel file don't observed")
         print("excel file don't observed")
     #두번째 파일
     try:
-        newexcel = pd.DataFrame()
+        newFrame = pd.DataFrame()
         #엑셀 모든 시트를 합쳐서 받는다.
-        newexcel = pd.read_excel(f'./input/{FileList[1]}', sheet_name=None, dtype=str)
-
+        newFrame = pd.read_excel(f'./input/{excelList[1]}', sheet_name=None, dtype=str)
     except:
         #엑셀파일 인식이 안된다면, 에러메세지 띄우기
-        msbox.showwarning("파일 인식 불가", f"input폴더의 {FileList[1]}파일을 확인해주세요.")
+        msbox.showwarning("파일 인식 불가", f"input폴더의 {excelList[1]}파일을 확인해주세요.")
         logger.debug("excel file don't observed")
         print("excel file don't observed")
 
-
-
-
-#######비교
+    resultFrame = newFrame.copy()
 
     time = str(datetime.datetime.now())[0:-7]
-    _text.insert(END, f"\n[{time}] 데이터 변경점 검색을 시작합니다.\n")
+    _text.insert(END, f"\n[{time}] 데이터 변경점 탐색을 시작합니다.\n")
 
     start = Time.time()
 
 
-#################공구리예정############################
+    #데이터가 다른 end data와 new data의 column, row 저장(append로)
+    coreRowNum = []
+    coreRowNum.clear()
+    coreRowNumPrint = []
+    coreRowNumPrint.clear()
+
+    columnList = endFrame.columns.tolist()
     #dataframe을 dictionary type으로 변환
-    b_dict = endexcel.to_dict("records")
-    a_dict = newexcel.to_dict("records")
+    b_dict = endFrame.to_dict("list")
+    a_dict = newFrame.to_dict("list")
+    rowList = len(a_dict)
     i = 0
     j = 0
-    for b in b_dict:
+    for comCol in columnList:
         j = 0
-        for a in a_dict:
+        bList = b_dict[f'{comcol}']
+        aList = a_dict[f'{comCol}']
+        for comRow in range(rowList):
             ######비교 로직 작성
-            if str(b['피보험자']) == str(a['피보험자']):
-                if str(b['모집인명']) == str(a['모집인명']):
-                    if str(b['피보험자\n주민등록번호'])[:5] == str(a['피보험자\n주민등록번호'])[:5]:
-                        if str(b['모집인 주민번호'])[:5] == str(a['모집인 주민번호'])[:5]:
-                            coreRowNum.append([i, j])
-                            coreRowNumPrint.append([i+1, j+1])
+            if bList[comRow] != aList[comRow]:
+                coreRowNum.append([i, j])
+                coreRowNumPrint.append([i+1, j+1])
             ######
             j = j + 1
         i = i + 1
@@ -89,28 +90,25 @@ def xlCompare(_text=None):
     end = Time.time()
     logger.info("excute time >>>>> " + str(end - start) + "s")
 
-    logger.debug("계약일 제외하고 일치하는 리스트")
+    logger.debug("변경된 데이터 리스트")
     logger.debug(coreRowNumPrint)
     logger.debug(len(coreRowNumPrint))
 
-    coreRowNumPrint = pd.DataFrame(coreRowNumPrint, columns=['전','후'])
+    #데이터 이격 생긴 부분 컬럼, 로우만 갈무리 해서 엑셀 생성
+    coreRowNumPrint = pd.DataFrame(coreRowNumPrint, columns=['column','row'])
     coreRowNumPrint.to_excel('./output/excel_tryout_rows.xlsx')
 
-    #개발자용
-    lastExcel = pd.DataFrame(lastList, columns=['전','후', '날짜 차이'])
-    logger.debug(lastExcel)#개발자용이기때문에 log에 남길필요 없음
-    #사용자용
-    lastExcelPrint = pd.DataFrame(lastListPrint, columns=['전','후', '날짜 차이'])
-    lastExcelPrint.to_excel('./excel_result/excel_final_rows.xlsx', index=False)
-    ###############공구리예정###################
-
+    #i=column, j=row
+    for i, j in coreRowNum:
+        y_color = painting(start_color='ffff99', end_color='ffff99', fill_type='solid')
+        resultFrame.cell(j,i).fill = y_color
 
 
     time = str(datetime.datetime.now())[0:-7]
     _text.insert(END, f"[{time}] 확인된 공통계약 리스트\n{lastExcelPrint}\n")
 
     finishStamp = str(datetime.datetime.now())[0:10]
-    finalExcel.to_excel(f'./output/{FileList[0],FileList[1]-finishStamp}.xlsx')
+    resultFrame.to_excel(f'./output/{excelList[0],excelList[1]-finishStamp}.xlsx')
     logger.info("compare complete")
     print("Excel file compare complete")
     time = str(datetime.datetime.now())[0:-7]
