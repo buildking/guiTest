5189d4d4482baab2433e74e7e17ff7de075eb19f
buildking <qtbelt@gmail.com>
2021-12-06 14:09:07 +0900

    delete

--- main.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-# # _tt = "2021-01-01".split("-")
-# # print(_tt[0])
-#
-# excel_date = 43891
-# dt = datetime.datetime.fromordinal(datetime.datetime(1900, 1, 1).toordinal() + excel_date - 2)
-# print(dt)
